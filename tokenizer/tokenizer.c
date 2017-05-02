bc9ddff54be04401cbe2bda13a5f033cdfb1e0dc
Kevin Christianson <kevinchristianson@eduroam-173-41.dyn.carleton.edu>
2017-05-02 17:46:22 -0500

    created displayTokens and edited tokenizer.c (especially string usage)

--- tokenizer/tokenizer.c
+++ tokenizer/tokenizer.c
@@ -1,6 +1,10 @@
 #include "value.h"
 #include "tokenizer.h"
 #include <string.h>
+#include "linkedlist.h"
+#include "talloc.h"
+#include <stdio.h>
+#include <assert.h>
 
 // Read all of the input from stdin, and return a linked list consisting of the
 // tokens.
@@ -12,7 +16,6 @@ Value *tokenize(){
     char* letterSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     char* numberSet = "0123456789";
     while (charRead != EOF) {
-
         if (charRead == '(') {
             Value *val = talloc(sizeof(Value));
             val->type = OPEN_TYPE;
@@ -30,12 +33,22 @@ Value *tokenize(){
         } else if (strchr(symbolSet, charRead)){
             Value *val = talloc(sizeof(Value));
             val->type = SYMBOL_TYPE;
-            char *temp = str[2];
-            temp[0] = charRead;
-            val->s = temp;
+            if(charRead == '#'){
+                val->s = talloc(3*sizeof(char));
+                val->s[0] = charRead;
+                charRead = fgetc(stdin);
+                if(charRead == 't' || charRead == 'f'){
+                    val->s[1] = charRead;
+                }else{
+                    printf("%s\n", "Syntax error: untokenizable");
+                }
+            }else{
+                val->s = talloc(2*(sizeof(char)));
+                val->s[0] = charRead;
+            }
             cons(val, list);
         } else if (strchr(letterSet, charRead)){
-            char *temp = str[256];
+            char *temp = talloc(100*sizeof(char));
             int counter = 0;
             while (charRead != ' '){
                 temp[counter] = charRead;
@@ -43,11 +56,11 @@ Value *tokenize(){
                 charRead = fgetc(stdin);
             }
             Value *val = talloc(sizeof(Value));
-            val->type = SYMBOL_TYPE;
+            val->type = STR_TYPE;
             val->s = temp;
             cons(val, list);
-        } else if(strchr(numberset, charRead)){
-            char *temp = str[256];
+        } else if(strchr(numberSet, charRead)){
+            char *temp = talloc(100*sizeof(char));
             int counter = 0;
             int flag = 0;
             while (charRead != ' '){
@@ -62,19 +75,18 @@ Value *tokenize(){
             Value *val = talloc(sizeof(Value));
             if (flag == 0){
                 val->type = INT_TYPE;
-                val->i = temp;
+                sscanf(temp, "%i", &val->i);
             } else {
-                val->type = DOUBLE_TYPE;
-                val->d = temp;
+                val->type = (double) DOUBLE_TYPE;
+                sscanf(temp, "%d", &val->i);
             }
             cons(val, list);
         }
         charRead = fgetc(stdin);
     }
-
     Value *revList = reverse(list);
     return revList;
-} 
+}
 
 /* Value *makeValue(valueType type, void *value){
     Value *val = talloc(sizeof(Value));
@@ -113,5 +125,35 @@ Value *tokenize(){
 
 // Displays the contents of the linked list as tokens, with type information
 void displayTokens(Value *list){
-
+    assert(list->type == CONS_TYPE);
+    while(list->type != CONS_TYPE){
+        switch (list->c.car->type) {
+            case INT_TYPE:
+                printf("%i%s", list->c.car->i, " :integer");
+                break;
+            case DOUBLE_TYPE:
+                printf("%f%s", list->c.car->d, " :float");
+                break;
+            case STR_TYPE:
+                printf("%s%s", list->c.car->s, " :string");
+                break;
+            case NULL_TYPE:
+                break;
+            case CONS_TYPE:
+                break;
+            case PTR_TYPE:
+                break;
+            case OPEN_TYPE:
+                printf("%s%s", list->c.car->s, " :open");
+            case CLOSE_TYPE:
+                printf("%s%s", list->c.car->s, " :close");
+            case BOOL_TYPE:
+                printf("%i%s", list->c.car->i, " :boolean");
+            case SYMBOL_TYPE:
+                printf("%s%s", list->c.car->s, " :symbol");
+        }
+        list = list -> c.cdr;
+        printf(")\n");
+  }
+  printf(")\n");
 }
