96e195c93e44df30f8501c9dfb602378cb72cf77
Kevin Christianson <kevinchristianson@res-235-163.dyn.carleton.edu>
2017-05-14 21:44:03 -0500

    added untested code for evalIf and evalLet

--- interpreter/interpreter.c
+++ interpreter/interpreter.c
@@ -1,5 +1,6 @@
 #include "interpreter.h"
-
+#include "linkedlist.h"
+#include "talloc.h"
 
 void interpret(Value *tree){
     Value *current = tree;
@@ -10,7 +11,7 @@ void interpret(Value *tree){
 }
 
 Value *lookUpSymbol(Value *expr, Frame *frame){
-    if(frame == NULL){
+    if(frame == NULL_TYPE){
         printf("Error: %s is not defined\n", car(expr)->s);
         texit(1);
     }
@@ -22,6 +23,52 @@ Value *lookUpSymbol(Value *expr, Frame *frame){
     return lookUpSymbol(expr, frame->parent);
 }
 
+// returns the number of tokens in an expression
+int checkParamNumber(Value *expr){
+    Value * current = expr;
+    int total_args = 0;
+    while(current->type != NULL_TYPE){
+        current = cdr(current);
+        total_args += 1;
+    }
+    return total_args;
+}
+
+Value *evalIf(Value *args, Frame *frame){
+    if(checkParamNumber(args) < 3){
+        printf("ERROR in IF statement: expected 3 parameters, got %i", checkParamNumber(args));
+        texit(1);
+    }
+    //if there are enough arguements, do if statement
+    if(eval(args)){
+        return cdr(args);
+    }
+    return cdr(cdr(args));
+}
+
+Value *evalLet(Value *args, Frame *frame){
+    if(checkParamNumber(args) < 2){
+        printf("ERROR in LET statement: expected 2 parameters, got %i", checkParamNumber(args));
+        texit(1);
+    }
+    Frame *new = talloc(sizeof(Frame));
+    new->bindings = makeNull();
+    new->parent = frame;
+    //assigns is the sub-tree containing all variable declarations
+    assigns = car(args)
+    while(assigns->type != NULL_TYPE){
+        if(checkParamNumber(car(assigns) != 2){
+            printf("ERROR in LET assignment statement: expected 2 parameters, got %i", checkParamNumber(args));
+            texit(1);
+        }
+        cons(car(assigns)->s, new);
+        cons(eval(cdr(car(assigns)), frame), new);
+        assigns = cdr(assigns);
+    }
+    return eval(cdr(args), new);
+
+}
+
 Value *eval(Value *expr, Frame *frame){
     switch (expr->type)  {
         case INT_TYPE: {
@@ -31,7 +78,7 @@ Value *eval(Value *expr, Frame *frame){
         case DOUBLE_TYPE: {
             return expr->d;
             break;
-        }    
+        }
         case STR_TYPE: {
             return expr->s;
             break;
@@ -63,17 +110,17 @@ Value *eval(Value *expr, Frame *frame){
             Value *first = car(expr);
             Value *args = cdr(expr);
             // Sanity and error checking on first...
-            
+
             if(first->type != SYMBOL_TYPE){
                 printf("ERROR: Attempting to evaluate a non-string as a special form!/n");
                 texit(1);
             }
             if (!strcmp(first->s,"if")) {
-                result = evalIf(args,frame);
+                result = evalIf(args, frame);
             }
-            
+
             if (!strcmp(first->s,"let")){
-                result = evalLet(args,frame);
+                result = evalLet(args, frame);
             }
             else {
                 // not a recognized special form
@@ -81,5 +128,5 @@ Value *eval(Value *expr, Frame *frame){
             }
             break;
         }
-    }    
+    }
 }
